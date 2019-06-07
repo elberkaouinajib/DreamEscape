01de8d490f858b9c41d6b45196f9d99b14bd0b83
Najib EL BERKAOUI <najib.el.berkaoui@gmail.com>
2019-06-07 14:49:25 +0200

    BAT crazy move

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class GameMaster : MonoBehaviour
+{
+    public int coins;
+    public TextMeshProUGUI coinsText;
+
+    private void Start() {
+        
+    }
+
+    private void Update() {
+        coinsText.text=("Coins : "+coins);
+    }
+}
--- Assets/Scripts/m1Move.cs
+++ Assets/Scripts/m1Move.cs
@@ -4,17 +4,42 @@ using UnityEngine;
 
 public class m1Move : MonoBehaviour
 {
-    float timer=0;
-    float waitTime=1.0f;
 
+
+	public float moveSpeed = 6f;
+
+	public float frequency = 10f;
+
+	public float magnitude = 1f;
+
+
+    public string batMove;
+     Vector3 pos;
+     private void Start() {
+          pos=transform.position;
+     }
 
     // Update is called once per frame
     void Update()
     {
-         transform.position=transform.position-(new Vector3(0.1f,0,0));
+         switch (batMove)
+         {
+             case "Crazy":CrazyMove(); break;
+             default: NormalMove(); break;
+         }
     }
     private void OnTriggerEnter2D(Collider2D other) {
          if(other.gameObject.tag=="Player")
             GameOverDetect.GameOver();
+    }
+
+    private void NormalMove(){
+         transform.position=transform.position-(new Vector3(0.1f,0,0));
+        
+    }
+
+    private void CrazyMove(){
+		pos -= transform.right * Time.deltaTime * moveSpeed;
+		transform.position = pos + transform.up * Mathf.Sin(Time.time * frequency) * magnitude;
     }
 }
